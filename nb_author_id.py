406112f767e98ac4f17b7a3c11393952d268d12a
Perham <perhamm@janelia.hhmi.org>
2019-01-02 12:54:56 -0500

    reinitialized commit

--- nb_author_id.py
+++ nb_author_id.py
@@ -29,10 +29,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0,3),"s"
+t0 = time()
 pred = clf.predict(features_test)
+print "predicting time: ", round(time()-t0,3),"s"
 accuracy = accuracy_score(pred, labels_test)
-print(accuracy)
+print "accuracy of Naive Bayes prediction: ", accuracy
 
 
 
